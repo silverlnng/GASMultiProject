072b35c337c895ed1ce4b8aa8ca7ae99688202aa
yjkwak <silverlnng0721@gmail.com>
2025-08-03 12:00:34 +0900

    변수 replicate 작업

--- Source/YJ_GASProject/GAS/MainAttributeSet.cpp
+++ Source/YJ_GASProject/GAS/MainAttributeSet.cpp
@@ -2,4 +2,35 @@
 
 
 #include "MainAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+void UMainAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue)
+{
+	// 매크로 사용
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMainAttributeSet,Health,OldValue);
+}
+
+void UMainAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMainAttributeSet,MaxHealth,OldValue);
+}
+
+void UMainAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMainAttributeSet,Mana,OldValue);
+}
+
+void UMainAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UMainAttributeSet,MaxMana,OldValue);
+}
+
+void UMainAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UMainAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMainAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMainAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UMainAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
--- Source/YJ_GASProject/GAS/MainAttributeSet.h
+++ Source/YJ_GASProject/GAS/MainAttributeSet.h
@@ -24,16 +24,27 @@ public:
 	ATTRIBUTE_ACCESSORS(UMainAttributeSet, MaxHealth)
 	ATTRIBUTE_ACCESSORS(UMainAttributeSet, Mana)
 	ATTRIBUTE_ACCESSORS(UMainAttributeSet, MaxMana)
+	virtual void GetLifetimeReplicatedProps( TArray< class FLifetimeProperty > & OutLifetimeProps ) const override;
 private:
-	UPROPERTY()
+	UPROPERTY(ReplicatedUsing=OnRep_Health)
 	FGameplayAttributeData Health;
 	
-	UPROPERTY()
+	UPROPERTY(ReplicatedUsing=OnRep_MaxHealth)
 	FGameplayAttributeData MaxHealth;
 
-	UPROPERTY()
+	UPROPERTY(ReplicatedUsing=OnRep_Mana)
 	FGameplayAttributeData Mana;
 	
-	UPROPERTY()
+	UPROPERTY(ReplicatedUsing=OnRep_MaxMana)
 	FGameplayAttributeData MaxMana;
+
+	// 이게 call 될때, 서버는 이미 값이 변경된 상태
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldValue);
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldValue);
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldValue);
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldValue);
 };
